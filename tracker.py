ff66ab3cdaf6c48a55833cc7d2f0b7d83ba6dc89
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-08-23 18:30:34 +0100

    first commit

--- tracker.py
+++ tracker.py
@@ -1,1 +1,13 @@
+from fredapi import Fred
 
+fredapi = Fred(api_key='43014c898148b01637a1c1150e17ff0c')
+
+# Treasury and Agency Securities: Mortgage-Backed Securities (MBS), All Commercial Banks
+mbsbankdata = fredapi.get_series_latest_release('TMBACBW027SBOG')
+mbsbankdata.tail()
+print(mbsbankdata)
+
+# Latest Mortgage 
+mort30yeardata = fredapi.get_series_latest_release('MORTGAGE30US')
+mort30yeardata.tail()
+print(mort30yeardata)
